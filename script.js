197227d99a923a1bc52008739053ece9d6aaea2a
KEATTLoevkilde <thom4a14@stud.kea.dk>
2023-09-12 09:14:24 +0200

    first commit game done

---
+++ script.js
@@ -0,0 +1,66 @@
+//Inputs
+const inpGuess = document.querySelector(".guess");
+
+//Buttons
+const buttonAgain = document.querySelector(".again");
+buttonAgain.addEventListener('click', resetGame)
+
+const buttonCheck = document.querySelector('.check')
+buttonCheck.addEventListener('click', checkGuess)
+
+//Labels
+const labelNumber = document.querySelector(".number");
+const labelScore = document.querySelector(".score");
+const labelHighScore = document.querySelector('.highscore')
+const labelMessage = document.querySelector(".message");
+
+//Variables
+let number = 0
+let score = 20
+let highscore = 0
+
+resetGame()
+
+function resetGame(){
+    number = Math.floor(Math.random() * 20 + 1)
+    score = 20
+    inpGuess.value = ""
+    labelNumber.textContent = '?'
+    labelScore.textContent = score
+    labelHighScore.textContent = highscore
+    labelMessage.textContent = 'Start guessing...'
+    console.log('number: ' + number)
+}
+
+function checkGuess(){
+    score = score - 1
+    labelScore.textContent = score
+    if(score <= 0){
+        labelMessage.textContent = "GAME OVER! Press 'Again' for new game"
+        labelScore.textContent = 0
+    }else if(parseInt(inpGuess.value) === number){
+        updateHighscore()
+    }else{
+        labelMessage.textContent = 'Guess again'
+    }
+}
+
+function updateHighscore(){
+    if(newHighscore()){
+        labelNumber.textContent = number
+        labelMessage.textContent = "You Won! New highscore!!! \nPress 'Again' for new game."
+    }else{
+        labelNumber.textContent = number
+        labelMessage.textContent = "You Won!\nPress 'Again' for new game."
+    }
+}
+
+function newHighscore(){
+    if(score > highscore){
+        highscore = score
+        return true
+    }else {
+        return false;
+    }
+}
+
